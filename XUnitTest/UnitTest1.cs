250ab71a73f079eb081ed048df13f22402bb0aa6
Collin Hintzke <collinhintzke5@gmail.com>
2018-06-11 22:23:30 -0700

    just need testing

---
+++ XUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static TicTacToe.Program;
+
+namespace XUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
